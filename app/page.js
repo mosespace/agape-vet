41a63d82577c16c94627d8292903fa89c163c990
mosespace <kisakyemoses50@gmail.com>
2023-09-20 14:27:53 +0300

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import CallToAction from "@/components/CallToAction";
 export default function Home() {
   return (
-    <main className='min-w-full lg:flex lg:flex-col lg:items-center lg:justify-between lg:p-[3rem] pt-[4.7rem] m-0 p-0'>
+    <main className='min-w-full pt-[4.5rem] m-0 p-0'>
       <CallToAction />
     </main>
   );
