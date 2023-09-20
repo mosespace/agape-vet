b0b87422ab1b18a1c9aab58bb18943c63157f4cc
mosespace <kisakyemoses50@gmail.com>
2023-09-20 16:27:31 +0300

    feat: added new component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Faq from "@/components/Faq";
 import CallToAction from "@/components/CallToAction";
 export default function Home() {
   return (
     <main className='min-w-full pt-[4.5rem] m-0 p-0'>
+      <Faq />
       <CallToAction />
     </main>
   );
