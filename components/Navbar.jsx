43d520b61c886fcafc4c4f9a723f46fb9ad087f9
mosespace <kisakyemoses50@gmail.com>
2023-09-21 15:37:27 +0300

    feat: initial commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -48,8 +48,8 @@ export default function Navbar() {
             className={`${
               menuOpen
                 ? "flex flex-col justify-between absolute py-4 gap-[3rem] font-medium text-slate-950 w-full md:w-[100vw] px-[3rem] top-[77px] transition-all ease-in-out duration-500 bg-slate-200 z-[1] md:z-auto md:left-0"
-                : "hidden lg:flex lg:flex-row lg:items-center dark:lg:text-slate-950 lg:text-slate-50 "
-            }lg:flex lg:items-center lg:flex-row gap-[3rem] font-medium dark:lg:text-slate-950 text-slate-50 w-full left-0 md:w-auto px-[3rem] transition-all ease-in-out duration-500`}
+                : "hidden lg:flex lg:flex-row lg:items-center lg:text-slate-950 dark:lg:text-slate-50 "
+            }lg:flex lg:items-center lg:flex-row gap-[3rem] font-medium w-full left-0 md:w-auto px-[3rem] transition-all ease-in-out duration-500`}
           >
             <li className='hover:border-b-2 hover:border-agape-yellow'>
               <Link href='#'>Home</Link>
