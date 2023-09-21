ce35f7e65b11f9f615e88b0ba799703230f9120b
mosespace <kisakyemoses50@gmail.com>
2023-09-21 15:37:12 +0300

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Faq from "@/components/Faq";
+import LearnMore from "@/components/LearnMore";
 import CallToAction from "@/components/CallToAction";
 export default function Home() {
   return (
     <main className='min-w-full pt-[4.5rem] m-0 p-0'>
+      <LearnMore />
       <Faq />
       <CallToAction />
     </main>
